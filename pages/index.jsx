d5f5e35ab518e90c071ff06970d5c2383aa9de3a
@oumar1994 <oumarfilston@gmail.com>
2023-01-26 18:37:08 -0800

    create header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Header from 'components/Header'
 export default function Home() {
   return (
     <>
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-   <h1>Hello World</h1>
+<Header/>
     </>
   )
 }
