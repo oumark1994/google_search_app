d492e0d41ecc80dbaa1b3d332d10fb0f8d720aa7
@oumar1994 <oumarfilston@gmail.com>
2023-01-29 00:48:58 -0800

    add authentication with google

--- components/User.js
+++ components/User.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import {useSession,signIn,SignOut} from 'next-auth/react'
 
 const User = () => {
+  const {data:session} = useSession();
+  if(session){
+    return (
+      <>
+      <img src={session.user.image} className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1  bg-gray-200 " onClick={SignOut }/>
+      </>
+    ) 
+  }
   return (
-    <div>User</div>
+    <><button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md" onClick={signIn}>Sign in</button></>
+    
   )
 }
 
