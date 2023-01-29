f7eec11e3ac7b80782e07a71a0bf687832a6f079
@oumar1994 <oumarfilston@gmail.com>
2023-01-29 04:19:57 -0800

    add body section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Header from 'components/Header'
+import Image from 'next/image'
+import {SearchIcon,MicrophoneIcon} from '@heroicons/react/solid'
 export default function Home() {
   return (
     <>
@@ -10,6 +12,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 <Header/>
+<form  className="flex flex-col items-center mt-50">
+  <Image width="300" objectFit='cover' height="100" src="https://techcrunch.com/wp-content/uploads/2015/10/screen-shot-2015-10-08-at-4-20-16-pm.png"/>
+  <div className=" flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg py-3 px-5 rounded-full items-center sm:max-w-lg lg:max-w-2xl ">
+    <SearchIcon className="h-5 text-gray-500 mr-3"/>
+    <input type="text" className='flex-grow focus:outline-none'/>
+    <MicrophoneIcon className="h-5 "/>
+  </div>
+  <div className='flex flex-col sm:flex-row w-[50%] space-y-3 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
+  <button className='btn'>Google Search</button>
+  <button className='btn'>I'm feeling lucky</button>
+  </div>
+  
+</form>
     </>
   )
 }
