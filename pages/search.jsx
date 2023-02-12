ac4ee9ba9dfaa64a588951c24cb6a1ce9dd76a51
@oumar1994 <oumarfilston@gmail.com>
2023-02-11 23:52:37 -0800

    change search to Search

--- pages/search.jsx
+++ pages/search.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Head from 'next/head'
 import SearchHeader from '../components/SearchHeader'
 import SearchResults from '../components/SearchResults'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import ImageResult from '../components/imageResult'
 
 
-const search = ({results}) => {
+const Search = ({results}) => {
+  const router = useRouter();
+
   // console.log(results)
-  const router = useRouter()
   return (
     <div>
         <Head>
@@ -25,7 +26,7 @@ const search = ({results}) => {
   )
 }
 
-export default search
+export default Search
 export async function getServerSideProps(context){
   const startIndex = context.query.start || "1"
   const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}`)
