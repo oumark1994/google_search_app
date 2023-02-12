8af54aaf016b586165b947c9719577627ad75172
@oumar1994 <oumarfilston@gmail.com>
2023-02-11 23:29:30 -0800

    change image to img

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -9,7 +9,7 @@ const signin = ({providers}) => {
     <div className="mt-50">
         {Object.values(providers).map(provider=>(
             <div key={provider.name} className="flex flex-col items-center">
-                <img className='w-52 object-cover' src="https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw" alt='' />
+                <Image className='w-52 object-cover' src="https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw" alt='' />
             <p className="text-sm italic my-10 text-center">This website is created for learning purposes</p>
             <button className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500" onClick={()=>signIn(provider.id,{callbackUrl:"/"})}>Sign in with {provider.name}</button>
             </div>
